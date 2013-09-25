e0ba885f849f71ca2968aefd6158404edb419797
Iskender TOTOĞLU <iskender@altivebir.com.tr>
2013-09-25 13:32:06 +0300

    Not need files deleted.

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@ $config['module']['comments'] = array
 (
     'module'        => "Comments",
     'name'          => "Comments",
-    'description'   => "Comment modules for managing website comments.<br /><b>Authors : </b>Pascal Gay, Victor Efremov, Laurent Brugière, İskender TOTOĞLU",
-    'author'        => "Pascal Gay, Victor Efremov, Laurent Brugière, İskender TOTOĞLU",
-    'version'       => "0.2a | Release //=> ukyo",
+    'description'   => "Comments Module help you to manage comments for articles.<br/><b>Author : </b>İskender TOTOĞLU<br/><b>Company : </b>ALTI VE BIR IT.<br/><b>Website : </b>http://www.altivebir.com.tr",
+    'author'        => "İskender TOTOĞLU | ALTI VE BIR IT.",
+    'version'       => "0.9",
     'uri'           => 'comments',
     'has_admin'     => TRUE,
     'has_frontend'  => FALSE,
